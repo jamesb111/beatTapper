c2b6683e5950ee57f4e528e4364fb57d9df3fe81
James <jamesbosley123@gmail.com>
2019-09-29 15:22:01 -0400

    Added animations

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 window.addEventListener("load", () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
-
-    console.log(sounds[0]);
+    const visual = document.querySelector(".visual");
+    const colors = [
+        'limegreen',
+        'orange',
+        'violet',
+        'maroon',
+        'royalblue',
+        'aqua'
+    ];
 
     //lets start the sound
     pads.forEach((pad, index) => {
         pad.addEventListener("click", function() {
             sounds[index].currentTime = 0;
             sounds[index].play();
+            createBubble(index);
+        });
+    });
+    
+    //function that makes bubbles
+    const createBubble = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = "jump 1s ease";
+        bubble.addEventListener("animationend", function() {
+            visual.removeChild(this);
         })
-    })
-})
+    }
+});
